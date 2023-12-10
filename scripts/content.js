8e2d5d471123ff5bb5605b314f64ebe5f5248cb7
Csaba Varga <varga.csaba.portfolio@gmail.com>
2023-12-10 14:20:06 +0100

    Fixed issues, and added npm and lint-staged

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,15 @@
 window.addEventListener('load', () => {
-  const videoElement = document.querySelector('.html5-video-container > video');
-  const adModule = document.querySelector('.ytp-ad-module');
-  
-  (new MutationObserver(() => {
-    videoElement.currentTime = 10000;
-  })).observe(adModule, { attributes: true, childList: true });
+  setTimeout(() => {
+    const videoElement = document.querySelector('.html5-video-container > video');
+    const adModule = document.querySelector('.ytp-ad-module');
+
+    if (adModule.children.length > 0) {
+      videoElement.currentTime = 10000;
+    }
+    
+    (new MutationObserver(() => {
+      console.log(videoElement.currentTime);
+      videoElement.currentTime = 10000;
+    })).observe(adModule, { attributes: true, childList: true });
+  }, 100)
 })
