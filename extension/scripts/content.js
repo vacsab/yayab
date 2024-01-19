8ddcda6c148537be82033d1356eb99b1ebabbbd4
Csaba Varga <varga.csaba.portfolio@gmail.com>
2024-01-19 01:21:54 +0100

    fixed some issues

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -3,13 +3,14 @@ window.addEventListener('load', () => {
     const videoElement = document.querySelector('.html5-video-container > video');
     const adModule = document.querySelector('.ytp-ad-module');
 
-    if (adModule.children.length > 0) {
+    if (adModule?.children?.length > 0) {
       videoElement.currentTime = 10000;
     }
     
-    (new MutationObserver(() => {
-      console.log(videoElement.currentTime);
-      videoElement.currentTime = 10000;
+    (new MutationObserver((records) => {
+      if (records.some((record) => record.attributeName === "display" && record.oldValue === "none") || adModule.style.display !== "none") {
+        videoElement.currentTime = 10000;
+      }
     })).observe(adModule, { attributes: true, childList: true });
   }, 100)
 })
